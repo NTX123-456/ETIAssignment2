7d8ba2389e4315ec292755bdb9e8c0811e9f5b6e
NTX123-456 <s10205325@connect.np.edu.sg>
2023-02-10 10:28:32 +0800

    Added docker connection

--- HotelConsole/HotelConsole.go
+++ HotelConsole/HotelConsole.go
@@ -6,15 +6,36 @@ import (
 	"database/sql"
 	"fmt"
 
-	//"net/http"
-	//"os"
+	"log"
+	"net/http"
+	"os"
 	"strings"
+
 	//"io/ioutil"
 	//"encoding/json"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
+	log.Print("starting server...")
+	http.HandleFunc("/", handler)
+
+	// Determine port for HTTP service.
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("defaulting to port %s", port)
+	}
+
+	// Start HTTP server.
+	log.Printf("listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
 outer:
 	for {
 		fmt.Println(strings.Repeat("=", 10))
